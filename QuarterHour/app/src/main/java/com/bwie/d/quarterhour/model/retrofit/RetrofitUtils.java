166f6853a0da5007f731b60c20ef3ac3e971c139
DeveloperSirLi <954322555@qq.com>
2018-01-19 23:00:38 +0800

    李棋文

---
+++ QuarterHour/app/src/main/java/com/bwie/d/quarterhour/model/retrofit/RetrofitUtils.java
@@ -0,0 +1,28 @@
+package com.bwie.d.quarterhour.model.retrofit;
+
+import com.bwie.d.quarterhour.view.IView.APIService;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+/**
+ * Created by d on 2018/1/19.
+ */
+
+public class RetrofitUtils {
+
+    private static APIService service = null;
+    public static APIService getInstance(){
+        if(service==null){
+            synchronized (RetrofitUtils.class){
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("")
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .build();
+
+                service = retrofit.create(APIService.class);
+            }
+        }
+        return service;
+    }
+}
