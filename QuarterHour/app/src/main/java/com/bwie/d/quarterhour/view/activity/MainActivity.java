fd5a212aa57120e14da45dddbf2b6dd2e6e390fd
DeveloperSirLi <954322555@qq.com>
2018-01-19 21:10:10 +0800

    李棋文

--- QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
+++ QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
@@ -3,26 +3,37 @@ package com.bwie.d.quarterhour.view.activity;
 import android.os.Bundle;
 
 import com.bwie.d.quarterhour.R;
+import com.bwie.d.quarterhour.presenter.BasePresenter;
 
 public class MainActivity extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     void initView() {
-
+    //初始化控件
     }
 
     @Override
     void initData() {
-//李棋文
+    //初始化数据
     }
 
     @Override
     int setContentViewId() {
+        //指定引用布局
         return R.layout.activity_main;
+    }
+
+    @Override
+    public BasePresenter initPresenter() {
+        //初始化Presenter
+        return null;
     }
 }
