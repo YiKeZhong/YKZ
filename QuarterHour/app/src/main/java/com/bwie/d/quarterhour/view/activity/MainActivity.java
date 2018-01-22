baa44f4425243082805bf9a05ab1c020d4f0b383
DeveloperSirLi <954322555@qq.com>
2018-01-22 14:13:19 +0800

    李棋文

--- QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
+++ QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
@@ -1,21 +1,9 @@
 package com.bwie.d.quarterhour.view.activity;
 
-import android.net.Uri;
 import android.os.Bundle;
-
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.view.View;
-import android.widget.Button;
 
 import com.bwie.d.quarterhour.R;
 import com.bwie.d.quarterhour.presenter.BasePresenter;
-import com.bwie.d.quarterhour.view.SlideMenu;
-import com.bwie.d.quarterhour.view.fragment.CrossTalkFragment;
-import com.bwie.d.quarterhour.view.fragment.Funnyfragment;
-import com.bwie.d.quarterhour.view.fragment.RecommendFragment;
-import com.bwie.d.quarterhour.view.fragment.VideoFragment;
-import com.facebook.drawee.view.SimpleDraweeView;
 
 public class MainActivity extends BaseActivity {
 
