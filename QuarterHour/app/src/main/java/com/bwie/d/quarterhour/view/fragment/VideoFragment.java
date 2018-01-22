0813b26f79d96c0656743195143fc661671260a5
DeveloperSirLi <954322555@qq.com>
2018-01-22 14:40:06 +0800

    李棋文

--- QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/fragment/VideoFragment.java
+++ QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/fragment/VideoFragment.java
@@ -3,19 +3,24 @@ package com.bwie.d.quarterhour.view.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bwie.d.quarterhour.R;
+import com.bwie.d.quarterhour.presenter.BasePresenter;
 
 
 /**
  * Created by weicy on 2018/1/20.
  */
 
-public class VideoFragment extends Fragment {
+public class VideoFragment extends BaseFragment {
+    @Override
+    public BasePresenter initPresenter() {
+        return null;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
