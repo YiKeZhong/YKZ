0b95101ad6b8eaad55a9bbb39bafa40ece8a0743
weicy <1300110011@qq.com>
2018-01-22 14:07:29 +0800

    weicy

--- QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
+++ QuarterHour/app/src/main/java/com/bwie/d/quarterhour/view/activity/MainActivity.java
@@ -1,13 +1,31 @@
 package com.bwie.d.quarterhour.view.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
 
 import com.bwie.d.quarterhour.R;
 import com.bwie.d.quarterhour.presenter.BasePresenter;
+import com.bwie.d.quarterhour.view.SlideMenu;
+import com.bwie.d.quarterhour.view.fragment.CrossTalkFragment;
+import com.bwie.d.quarterhour.view.fragment.Funnyfragment;
+import com.bwie.d.quarterhour.view.fragment.RecommendFragment;
+import com.bwie.d.quarterhour.view.fragment.VideoFragment;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class MainActivity extends BaseActivity {
 
 
+    private SimpleDraweeView draweeView;
+    private SlideMenu slideMenu;
+    private Button radio_01;
+    private Button radio_02;
+    private Button radio_03;
+    private Button radio_04;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +36,92 @@ public class MainActivity extends BaseActivity {
     @Override
     void initView() {
     //初始化控件
+        draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
+        slideMenu = (SlideMenu)findViewById(R.id.slideMenu);
+        //底部
+        radio_01 = findViewById(R.id.radio_01);
+        radio_02 = findViewById(R.id.radio_02);
+        radio_03 = findViewById(R.id.radio_03);
+        radio_04 = findViewById(R.id.radio_04);
+
     }
 
     @Override
     void initData() {
     //初始化数据
+        Uri uri = Uri.parse("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1517015634&di=064833d0a882df2da895b8ad87d2336a&imgtype=jpg&er=1&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2014%2F289%2F01%2FIGS09651F94M.jpg");
+        draweeView.setImageURI(uri);
+        //点击出侧拉
+        draweeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slideMenu.switchMenu();
+            }
+        });
+
+        RecommendFragment recommendFragment = new RecommendFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        radio_01.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.tuijian_02), null, null);
+        fragmentTransaction.replace(R.id.frameLayout, recommendFragment);
+        fragmentTransaction.commit();
+        //推荐
+        radio_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecommendFragment recommendFragment = new RecommendFragment();
+                radio_01.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.tuijian_02), null, null);
+                radio_02.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.duanzi_01), null, null);
+                radio_03.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.shipin_01), null, null);
+                radio_04.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.qutu_01), null, null);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, recommendFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        //段子
+        radio_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrossTalkFragment crossTalkFragment = new CrossTalkFragment();
+                radio_01.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.tuijian_01), null, null);
+                radio_02.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.duanzi_02), null, null);
+                radio_03.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.shipin_01), null, null);
+                radio_04.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.qutu_01), null, null);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, crossTalkFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        //视频
+        radio_03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoFragment videoFragment = new VideoFragment();
+                radio_01.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.tuijian_01), null, null);
+                radio_02.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.duanzi_01), null, null);
+                radio_03.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.shipin_02), null, null);
+                radio_04.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.qutu_01), null, null);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, videoFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        //趣图
+        radio_04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Funnyfragment funnyfragment = new Funnyfragment();
+                radio_01.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.tuijian_01), null, null);
+                radio_02.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.duanzi_01), null, null);
+                radio_03.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.shipin_01), null, null);
+                radio_04.setCompoundDrawablesWithIntrinsicBounds(null, ContextCompat.getDrawable(MainActivity.this, R.drawable.qutu_02), null, null);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, funnyfragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+
     }
 
     @Override
